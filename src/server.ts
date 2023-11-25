32a16556c534348c38c2255fdd0ab50c788af965
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-11-25 22:00:00 +0600

    [DONE] added express env mongoose

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+import config from "./config/index";
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`🛢 Database is connected & listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.error("Failed to connect database", err);
+  }
+}
+
+bootstrap();
