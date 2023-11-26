817ee27bcd83ff55a9c3ab1d1764e3606e51dd16
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-11-26 22:20:27 +0600

    [Done]: created user model and interface and improve eslint

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
@@ -8,9 +9,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-//handle not found
+// application routes
+app.use('/api/v1/users', usersRouter)
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send('Server is running!')
 })
 
