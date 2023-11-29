021d7102cf1c6a526396c7db92e889f9740e8035
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-11-29 20:49:54 +0600

    [DONE] create Academic semester route

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,27 @@
-import express, { Application } from 'express'
-import cors from 'cors'
-import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import { UserRoutes } from './app/modules/users/user.route'
-const app: Application = express()
+import express, { Application } from "express";
+import cors from "cors";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import { UserRoutes } from "./app/modules/user/user.route";
+import { AcademicSemesterRoutes } from "./app/modules/academicSemester/academicSemester.route";
 
-app.use(cors())
+const app: Application = express();
+
+app.use(cors());
 
 //parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // application routes
-app.use('/api/v1/users', UserRoutes)
+app.use("/api/v1/users", UserRoutes);
+app.use("/api/v1/academic-semesters", AcademicSemesterRoutes);
 
-app.get('/', async () => {
+app.get("/", async () => {
   // Promise.reject(new Error('Unhandled promise rejection'))
-  throw new Error('Unhandled error')
-})
+  throw new Error("Unhandled error");
+});
 
 // global error handler
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
-export default app
+export default app;
