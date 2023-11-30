d72a7f9e6b6c48053565c83eeffffcdc5b68bf60
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-11-30 13:58:27 +0600

    code optimized

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -8,6 +8,8 @@ import {
   academicSemesterMonths,
   academicSemesterTitles,
 } from "./academicSemester.constant";
+import ApiError from "../../../errors/ApiError";
+import httpStatus from "http-status";
 
 const academicSemesterSchema = new Schema<IAcademicSemester>(
   {
@@ -23,6 +25,21 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
   },
   { timestamps: true },
 );
+
+// handling same year and same semester issue
+academicSemesterSchema.pre("save", async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+  if (isExist) {
+    throw new ApiError(
+      httpStatus.CONFLICT,
+      "Academic Semester is already exist !",
+    );
+  }
+  next();
+});
 
 export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   "AcademicSemester",
