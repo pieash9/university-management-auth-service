ab4b1503aefce08c9a934ec504f56a667f61bfb7
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-12-01 16:04:08 +0600

    feat: [DONE] creating academic semester pagination

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import routes from "./app/routes";
+import httpStatus from "http-status";
 
 const app: Application = express();
 
@@ -14,12 +15,27 @@ app.use(express.urlencoded({ extended: true }));
 // application routes
 app.use("/api/v1", routes);
 
-app.get("/", async () => {
-  // Promise.reject(new Error('Unhandled promise rejection'))
-  throw new Error("Unhandled error");
-});
+// app.get("/", async () => {
+//   // Promise.reject(new Error('Unhandled promise rejection'))
+//   throw new Error("Unhandled error");
+// });
 
 // global error handler
 app.use(globalErrorHandler);
+
+// handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "Not Found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "API not found !",
+      },
+    ],
+  });
+  next();
+});
 
 export default app;
--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -3,21 +3,43 @@ import { AcademicSemesterService } from "./academicSemester.service";
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
+import pick from "../../../shared/pick";
+import { paginationFields } from "../../../constants/pagination";
+import { IAcademicSemester } from "./academicSemester.interface";
 
 const createSemester = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { ...academicSemesterData } = req.body;
     const result =
       await AcademicSemesterService.createSemester(academicSemesterData);
-    next();
 
-    sendResponse(res, {
+    sendResponse<IAcademicSemester>(res, {
       statusCode: httpStatus.OK,
       success: true,
-      message: "Academic Semester created successfully",
+      message: "Aademic semester created successfully!",
       data: result,
     });
+    next();
   },
 );
 
-export const AcademicSemesterController = { createSemester };
+const getAllSemesters = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const paginationOptions = pick(req.query, paginationFields);
+
+    console.log(paginationOptions);
+    const result =
+      await AcademicSemesterService.getAllSemesters(paginationOptions);
+
+    sendResponse<IAcademicSemester[]>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Semester retrieved successfully",
+      meta: result.meta,
+      data: result.data,
+    });
+    next();
+  },
+);
+
+export const AcademicSemesterController = { createSemester, getAllSemesters };
