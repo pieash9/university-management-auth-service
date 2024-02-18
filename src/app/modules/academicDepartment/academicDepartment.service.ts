002a0925246755b0b619f4bda37985d85096d2a7
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-02-18 10:44:05 +0600

    connected academic department with redis

--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -1,14 +1,16 @@
-import { IPaginationOptions } from '../../../interfaces/pagination';
-import { PaginationHelpers } from '../../../helpers/paginationHelper';
-
-import { IGenericResponse } from '../../../interfaces/common';
 import { SortOrder } from 'mongoose';
+import { PaginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import { AcademicFaculty } from '../academicFaculty/academicFaculty.model';
+import { academicDepartmentSearchableFields } from './academicDepartment.constant';
 import {
+  AcademicDepartmentCreatedEvent,
+  AcademicDepartmentUpdatedEvent,
   IAcademicDepartment,
   IAcademicDepartmentFilters,
 } from './academicDepartment.interface';
 import { AcademicDepartment } from './academicDepartment.model';
-import { academicDepartmentSearchableFields } from './academicDepartment.constant';
 
 const createDepartment = async (
   payload: IAcademicDepartment,
@@ -19,22 +21,40 @@ const createDepartment = async (
   return result;
 };
 
+const getSingleDepartment = async (
+  id: string,
+): Promise<IAcademicDepartment | null> => {
+  const result =
+    await AcademicDepartment.findById(id).populate('academicFaculty');
+
+  return result;
+};
+
 const getAllDepartments = async (
   filters: IAcademicDepartmentFilters,
   paginationOptions: IPaginationOptions,
 ): Promise<IGenericResponse<IAcademicDepartment[]>> => {
+  const { limit, page, skip, sortBy, sortOrder } =
+    PaginationHelpers.calculatePagination(paginationOptions);
+
+  // Extract searchTerm to implement search query
   const { searchTerm, ...filtersData } = filters;
 
   const andConditions = [];
 
+  // Search needs $or for searching in specified fields
   if (searchTerm) {
     andConditions.push({
       $or: academicDepartmentSearchableFields.map(field => ({
-        [field]: { $regex: searchTerm, $options: 'i' },
+        [field]: {
+          $regex: searchTerm,
+          $paginationOptions: 'i',
+        },
       })),
     });
   }
 
+  // Filters needs $and to fullfill all the conditions
   if (Object.keys(filtersData).length) {
     andConditions.push({
       $and: Object.entries(filtersData).map(([field, value]) => ({
@@ -43,15 +63,13 @@ const getAllDepartments = async (
     });
   }
 
-  const { limit, page, skip, sortBy, sortOrder } =
-    PaginationHelpers.calculatePagination(paginationOptions);
-
+  // Dynamic  Sort needs  field to  do sorting
   const sortConditions: { [key: string]: SortOrder } = {};
-
   if (sortBy && sortOrder) {
     sortConditions[sortBy] = sortOrder;
   }
 
+  // If there is no condition , put {} to give all data
   const whereConditions =
     andConditions.length > 0 ? { $and: andConditions } : {};
 
@@ -60,7 +78,8 @@ const getAllDepartments = async (
     .sort(sortConditions)
     .skip(skip)
     .limit(limit);
-  const total = await AcademicDepartment.countDocuments();
+
+  const total = await AcademicDepartment.countDocuments(whereConditions);
 
   return {
     meta: {
@@ -70,14 +89,6 @@ const getAllDepartments = async (
     },
     data: result,
   };
-};
-
-const getSingleDepartment = async (
-  id: string,
-): Promise<IAcademicDepartment | null> => {
-  const result =
-    await AcademicDepartment.findById(id).populate('academicFaculty');
-  return result;
 };
 
 const updateDepartment = async (
@@ -91,6 +102,7 @@ const updateDepartment = async (
       new: true,
     },
   ).populate('academicFaculty');
+
   return result;
 };
 
@@ -101,10 +113,51 @@ const deleteDepartment = async (
   return result;
 };
 
+const insertIntoDBFromEvent = async (
+  e: AcademicDepartmentCreatedEvent,
+): Promise<void> => {
+  const academicFaculty = await AcademicFaculty.findOne({
+    syncId: e.academicFacultyId,
+  });
+  const payload = {
+    title: e.title,
+    academicFaculty: academicFaculty?._id,
+    syncId: e.id,
+  };
+
+  await AcademicDepartment.create(payload);
+};
+
+const updateOneInDBFromEvent = async (
+  e: AcademicDepartmentUpdatedEvent,
+): Promise<void> => {
+  const academicFaculty = await AcademicFaculty.findOne({
+    syncId: e.academicFacultyId,
+  });
+  const payload = {
+    title: e.title,
+    academicFaculty: academicFaculty?._id,
+  };
+
+  await AcademicDepartment.findOneAndUpdate(
+    { syncId: e.id },
+    {
+      $set: payload,
+    },
+  );
+};
+
+const deleteOneFromDBFromEvent = async (syncId: string): Promise<void> => {
+  await AcademicDepartment.findOneAndDelete({ syncId });
+};
+
 export const AcademicDepartmentService = {
   createDepartment,
+  getSingleDepartment,
   getAllDepartments,
   updateDepartment,
-  getSingleDepartment,
   deleteDepartment,
+  insertIntoDBFromEvent,
+  updateOneInDBFromEvent,
+  deleteOneFromDBFromEvent,
 };
